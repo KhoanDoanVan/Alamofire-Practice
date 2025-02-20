d03dc25ea3f940a249d0c669af61107db7371417
khoanisdev <doanvankhoan124@gmail.com>
2025-02-20 14:20:35 +0700

    Initial Commit

---
+++ Alamofire-Practice/Alamofire_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alamofire_PracticeApp.swift
+//  Alamofire-Practice
+//
+//  Created by Đoàn Văn Khoan on 20/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Alamofire_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
